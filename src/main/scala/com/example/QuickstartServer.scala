74292b62f7f068f95f2a7828b31201fcee302e7b
Потапов Эдуард <edwpotapoff@gmail.com>
2025-01-14 17:54:21 +0300

    Ws Service in QuickstartServer -binary

--- src/main/scala/com/example/QuickstartServer.scala
+++ src/main/scala/com/example/QuickstartServer.scala
@@ -111,8 +111,9 @@ object QuickstartServer extends App with UserRoutes {
         TextMessage(Source.single("Hello ") ++ tm.textStream ++ Source.single("!")) :: Nil
       case bm: BinaryMessage =>
         // ignore binary messages but drain content to avoid the stream being clogged
-        bm.dataStream.runWith(Sink.ignore)
-        Nil
+        BinaryMessage(Source.single(ByteString("Hello ")) ++ bm.dataStream ++ Source.single(ByteString("!"))) :: Nil
+      //bm.dataStream.runWith(Sink.ignore)
+      //Nil
     }
   //#websocket-handler
   val requestHandler: HttpRequest => Future[HttpResponse] = {
